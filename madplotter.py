ad7efc6b5d8114a115f004307f3226aa8e4d1f54
Aleksandr Gorn <aleksandr.gorn@cern.ch>
2023-12-15 13:43:39 +0100

    first commit

---
+++ madplotter.py
@@ -0,0 +1,249 @@
+import holoviews as hv
+from holoviews import opts
+from bokeh.models import HoverTool
+from copy import copy
+from collections.abc import Iterable
+import numpy as np
+
+import warnings
+from pandas.errors import SettingWithCopyWarning
+warnings.simplefilter(action='ignore', category=(SettingWithCopyWarning))
+
+################## Lattice ##################
+colors = {
+    'quadrupole': 'mediumseagreen',
+    'sextupole': 'blue',
+    'octupole': 'red',
+    'bend': 'salmon',
+    'rbend': 'salmon',
+    'lbend': 'salmon',
+    'hkicker': 'purple',
+    'vkicker': 'orange',
+    'kicker': 'orange',
+    'marker': 'blue',
+    'rfcavity': 'wheat',
+    'collimator': 'black'
+}
+
+fontsize={
+    'title': 15, 
+    'labels': 14, 
+    'xticks': 10, 
+    'yticks': 10,
+}
+
+vdims_names = ['color', 'keyword', 'name', 's', 'apertype', 'aper_1', 'aper_2', 'l', 'k0l', 'k1l','betx', 'bety', 'alfx', 'alfy', 'mux', 'muy']
+tooltips = [(item, '@' + item) for item in vdims_names[1:]]
+hover = HoverTool(tooltips=tooltips)
+
+s_dim = hv.Dimension('s', unit='m')
+
+default_style = dict(
+    width=1400, 
+    height=300, 
+    color='color', 
+    line_color='black', 
+    line_width=0.5, 
+    tools=[hover, 'tap'],
+    fontsize=fontsize
+)
+
+def plot_lattice(
+        twiss,
+        range_=None,
+        vdims_names=vdims_names, elem_height=0.1,
+        color=colors, show_markers=False,
+        marker_width = 0.3, marker_height = 0.4,
+        twiss_plots=None,
+        offsets=None,
+        **kwargs):
+    
+    if range_:
+        range_ = range_.split('/')
+        twiss = copy(twiss.loc[range_[0]:range_[1]])
+        twiss.s -= twiss.loc[range_[0]].s
+    else:
+        twiss = copy(twiss)
+    
+    twiss['color'] = [colors[kwrd] if kwrd in colors.keys() else None for kwrd in twiss.keyword]
+    to_draw = twiss[(twiss['color'] >= '') & (twiss['aper_1'] > 0)]
+    vdims = [to_draw[item] for item in vdims_names]
+
+    to_draw['right'] = to_draw['s']
+    to_draw['s'] = to_draw['s'] - to_draw['l']
+    to_draw['xtopto'] = to_draw['aper_1'] + elem_height
+    to_draw['xtopfrom'] = to_draw['aper_1']
+    to_draw['xbotto'] = -to_draw['aper_1']
+    to_draw['xbotfrom'] = - to_draw['aper_1'] - elem_height
+    to_draw['ytopto'] = to_draw['aper_2'] + elem_height
+    to_draw['ytopfrom'] = to_draw['aper_2']
+    to_draw['ybotto'] = -to_draw['aper_2']
+    to_draw['ybotfrom'] = - to_draw['aper_2'] - elem_height
+
+    # Applying offsets
+    if offsets:
+        for key in offsets.keys():
+            if key not in to_draw.index:
+                continue
+            to_draw.loc[key, 'xtopfrom'] += offsets[key][0]
+            to_draw.loc[key, 'xbotto'] += offsets[key][0]
+            to_draw.loc[key, 'ytopfrom'] += offsets[key][1]
+            to_draw.loc[key, 'ybotto'] += offsets[key][1]
+    
+    # Delete repeating keys from default_style
+    style = copy(default_style)
+    for key in kwargs.keys():
+        style.pop(key, None)
+
+    options = opts.Rectangles(**style, **kwargs)
+
+    lattice_top_x = hv.Rectangles(to_draw, kdims=[s_dim, 'xtopfrom', 'right', 'xtopto'], vdims=vdims_names)
+    lattice_bottom_x = hv.Rectangles(to_draw, kdims=[s_dim, 'xbotfrom', 'right', 'xbotto'], vdims=vdims_names)
+    lattice_top_y = hv.Rectangles(to_draw, kdims=[s_dim, 'ytopfrom', 'right', 'ytopto'], vdims=vdims_names)
+    lattice_bottom_y = hv.Rectangles(to_draw, kdims=[s_dim, 'ybotfrom', 'right', 'ybotto'], vdims=vdims_names)
+
+    top_plot = lattice_top_x * lattice_bottom_x
+    bottom_plot = lattice_top_y * lattice_bottom_y
+    
+    if show_markers:
+        markers = twiss[twiss.keyword == 'marker']
+        vdims_markers = [markers[item] for item in vdims_names]
+        markers_drawn = hv.Rectangles((markers['s'] - marker_width/2, 
+                             -marker_height/2, 
+                             markers['s'] + marker_width/2, 
+                             marker_height/2,
+                               *vdims_markers                          
+                            ), vdims=vdims_names)
+        
+        top_plot *= markers_drawn
+        bottom_plot *= markers_drawn
+    
+    lattice = (top_plot).opts(ylabel='x (m)') \
+    + (bottom_plot).opts(ylabel='y (m)')
+
+    twiss_plots = get_twiss_plots(twiss, kind=twiss_plots)
+    for key in twiss_plots.keys():
+        lattice += twiss_plots[key]
+    
+    return lattice.opts(options).cols(1)
+
+
+################## Envelope ##################
+area_core_style = dict(
+    color = 'lightblue',
+    alpha = 0.7
+)
+
+area_wings_style = dict(
+    color = 'lightblue',
+    alpha = 0.2
+)
+
+curve_style = dict(
+    color = 'black',
+    line_width = 0.1,
+    tools = ['hover']
+)
+
+def plot_envelope(env, range_=None, area_core_style=area_core_style, area_wings_style=area_wings_style, curve_style=curve_style):
+    if range_:
+        cond = (env.s >= range_[0]) & (env.s <= range_[1])
+        env = copy(env[cond])
+        env.s -= range_[0]
+    
+    xp90 = hv.Area(env, s_dim, vdims=['xp5', 'xp95'], label='envelope-p90')
+    xp99 = hv.Area(env, s_dim, vdims=['xp0_5', 'xp99_5'], label='envelope-p99')
+    yp90 = hv.Area(env, s_dim, vdims=['yp5', 'yp95'], label='envelope-p90')
+    yp99 = hv.Area(env, s_dim, vdims=['yp0_5', 'yp99_5'], label='envelope-p99')
+
+    xp95 = hv.Curve(env, s_dim, 'xp5') * hv.Curve(env, s_dim, 'xp95')
+    xp995 = hv.Curve(env, s_dim, 'xp0_5') * hv.Curve(env, s_dim, 'xp99_5')
+    yp95 = hv.Curve(env, s_dim, 'yp5') * hv.Curve(env, s_dim, 'yp95')
+    yp995 = hv.Curve(env, s_dim, 'yp0_5') * hv.Curve(env, s_dim, 'yp99_5')
+
+    curve_opts = opts.Curve(**curve_style)
+    area_core_opts = opts.Area(**area_core_style)
+    area_wings_opts = opts.Area(**area_wings_style)
+
+    envelope_x = xp95 * xp995
+    envelope_y = yp95 * yp995
+
+    area_x = xp99.opts(area_wings_opts) * xp90.opts(area_core_opts)
+    area_y = yp99.opts(area_wings_opts) * yp90.opts(area_core_opts)
+    envelope = (area_x * envelope_x + area_y * envelope_y).opts(curve_opts).cols(1)
+    return envelope
+
+
+################## Twiss ##################
+default_twiss_keys = ['betx', 'bety', 'alfx', 'alfy', 'mux', 'muy']
+
+twiss_style = dict(
+    width=1400, 
+    height=300,  
+    tools=['hover', 'tap'],
+    fontsize=fontsize
+)
+
+def get_twiss_plots(twiss, kind='beta'):
+    # if range_:
+    #     range_ = range_.split('/')
+    #     twiss = copy(twiss.loc[range_[0]:range_[1]])
+    #     twiss.s -= twiss.loc[range_[0]].s
+    # else:
+    #     twiss = copy(twiss)
+    if not isinstance(kind, Iterable):
+        kind = [kind]
+    plots = {}
+    if 'beta' in kind:
+        beta = hv.Curve(twiss, s_dim, vdims=['betx', 'name'], label='betx') * hv.Curve(twiss, s_dim, vdims=['bety', 'name'], label='bety')
+        plots['beta'] = beta.opts(opts.Curve(**twiss_style)).opts(ylabel='beta (m)')
+    if 'alpha' in kind:
+        alpha = hv.Curve(twiss, s_dim, vdims=['alfx', 'name'], label='alfx') * hv.Curve(twiss, s_dim, vdims=['alfy', 'name'], label='alfy')
+        plots['alpha'] = alpha.opts(opts.Curve(**twiss_style)).opts(ylabel='alpha')
+    if 'mu' in kind:
+        twiss['mux_norm'] = twiss.mux * np.pi / 2.
+        twiss['muy_norm'] = twiss.muy * np.pi / 2.
+        mu = hv.Curve(twiss, s_dim, vdims=['mux_norm', 'name'], label='mux') * hv.Curve(twiss, s_dim, vdims=['muy_norm', 'name'], label='muy')
+        plots['mu'] = mu.opts(opts.Curve(**twiss_style)).opts(ylabel='2 mu / pi')
+    if 'd' in kind:
+        alpha = hv.Curve(twiss, s_dim, vdims=['dx', 'name'], label='Dx') * hv.Curve(twiss, s_dim, vdims=['dy', 'name'], label='Dy')
+        plots['D'] = alpha.opts(opts.Curve(**twiss_style)).opts(ylabel='D (m)')
+    return plots
+
+def plot_twiss(twiss, kind='beta', range_=None, **kwargs):
+    if range_:
+        range_ = range_.split('/')
+        twiss = copy(twiss.loc[range_[0]:range_[1]])
+        twiss.s -= twiss.loc[range_[0]].s
+    else:
+        twiss = copy(twiss)
+    plots = get_twiss_plots(twiss, kind=kind)
+    for i, key in enumerate(plots.keys()):
+        if i == 0:
+            layout = plots[key]
+        else:
+            layout += plots[key]
+    return layout.opts(**kwargs).cols(1)
+
+################## All ##################
+def plot_all(twiss, env, range_=None, twiss_plots=None, offsets=None, **kwargs):
+    if range_:
+        range_env=range_.split('/')
+        s1 = twiss.loc[range_env[0]].s
+        s2 = twiss.loc[range_env[1]].s
+        range_env= [s1, s2]
+    else:
+        range_env = None
+    lattice = plot_lattice(twiss, range_=range_, twiss_plots=twiss_plots, offsets=offsets, **kwargs)
+    envelope = plot_envelope(env, range_=range_env)
+    x_opts = dict(xlabel='s (m)', ylabel='x (m)')
+    y_opts = dict(xlabel='s (m)', ylabel='y (m)')
+
+    layout = (lattice[0] * envelope[0]).opts(**x_opts) + (lattice[1] * envelope[1]).opts(**y_opts)
+
+    for i in range(len(lattice)):
+        if i > 1:
+            layout += lattice[i]
+
+    return layout.cols(1)
+
---
+++ madtracker.py
@@ -0,0 +1,163 @@
+import numpy as np
+import pandas as pd
+from copy import copy
+
+def track_line_ptc(
+        madx,
+        particles_in,
+        sequence=False,
+        seq_range="#s/#e",
+        observe=None,
+        model=2,
+        method=4,
+        nst=4,
+        exact=True,
+        icase=5,
+        onetable=True,
+        recloss=True,
+        norm_no=4,
+        **kwargs,
+    ):
+        parts_in = particles_in.copy()
+        true_idx = np.array(parts_in.number)
+        if sequence:
+            madx.use(sequence=sequence, range_=seq_range)
+        madx.ptc_create_universe(ntpsa=True)
+        madx.ptc_create_layout(model=model, method=method, nst=nst, exact=exact)
+        madx.ptc_align()
+        # Tracking in ptc:
+
+        for idx, part in parts_in.iterrows():
+                madx.ptc_start(
+                    x=part.x,
+                    px=part.px,
+                    y=part.y,
+                    py=part.py,
+                    t=0.0,
+                    pt=part.pt,
+                )
+
+        if observe:
+            if isinstance(observe, (list, tuple)):
+                for ele in observe:
+                    madx.ptc_observe(place=ele)
+            else:
+                madx.ptc_observe(place=observe)
+
+        madx.ptc_track(
+            icase=icase,
+            onetable=onetable,
+            recloss=recloss,
+            norm_no=norm_no,
+            **kwargs,
+        )
+        madx.ptc_track_end()
+
+        try:
+            parts_out = madx.table.trackone.dframe()
+        except KeyError:
+            parts_out = pd.DataFrame()
+        else:
+            parts_out = parts_out[parts_out["turn"] == 1.0]
+            parts_out["number"] = [true_idx[int(n) - 1] for n in parts_out.number]
+        try:
+            parts_lost = madx.table.trackloss.dframe()
+        except KeyError:
+            parts_lost = pd.DataFrame()
+        else:
+            parts_lost["number"] = [true_idx[int(n) - 1] for n in parts_lost.number]
+
+        return parts_out, parts_lost
+
+def transfer_beam(
+        madx, 
+        particles_distribution,
+        init_cond,
+        n_particles = 5000,
+        observe='all',
+        sequence=None, 
+        range_=None,
+        ):
+    if sequence:
+        madx.use(sequence=sequence, range_=range_)
+    twiss = madx.twiss(**init_cond, rmatrix = True).dframe()
+    
+    if n_particles:
+        particles_distribution = particles_distribution.sample(n_particles)
+    particles_distribution["number"] = particles_distribution.index
+
+    if observe == 'all':
+        observe = list(twiss.index)
+    else:
+        observe = list(twiss.index)[-1]
+        
+    particles, losses = track_line_ptc(
+        madx=madx,
+        particles_in=particles_distribution,
+        observe=observe
+    )
+    return particles, twiss
+
+    
+def calc_envelope(particles):
+    locs = particles.index.unique()
+    d = dict(
+        s = [],
+        minx = [],
+        miny = [],
+        maxx = [],
+        maxy = [],
+        xp0_5 = [],
+        yp0_5 = [],
+        xp99_5 = [],
+        yp99_5 = [],
+        xp5 = [],
+        yp5 = [],
+        xp95 = [],
+        yp95 = [],
+        medx = [],
+        medy = []
+    )
+    for i, bsg in enumerate(locs):
+        try:
+            d['s'].append(particles.loc[bsg]['s'][0])
+            distx = particles.loc[bsg]['x'] - 0*np.mean(particles.loc[bsg]['x'])
+            disty = particles.loc[bsg]['y'] - 0*np.mean(particles.loc[bsg]['y'])
+            d['minx'].append(np.min(distx))
+            d['miny'].append(np.min(disty))
+            d['maxx'].append(np.max(distx))
+            d['maxy'].append(np.max(disty))
+            d['xp0_5'].append(np.percentile(distx, 0.5))
+            d['yp0_5'].append(np.percentile(disty, 0.5))
+            d['xp99_5'].append(np.percentile(distx, 99.5))
+            d['yp99_5'].append(np.percentile(disty,99.5))
+            d['xp5'].append(np.percentile(distx, 5))
+            d['yp5'].append(np.percentile(disty, 5))
+            d['xp95'].append(np.percentile(distx, 95))
+            d['yp95'].append(np.percentile(disty, 95))
+            d['medx'].append(np.median(distx))
+            d['medy'].append(np.median(disty))
+        except:
+            pass
+    return pd.DataFrame(d)
+
+def calc_beam_pars(beam, gamma=400 / 0.938, center_beam=True):
+    pars = {}
+    if center_beam:
+        beam = copy(beam) - beam.mean()
+    pars['sigma_x'] = np.sqrt((beam.x**2).mean())
+    pars['sigma_y'] = np.sqrt((beam.y**2).mean())
+    pars['sigma_xp'] = np.sqrt((beam.px**2).mean())
+    pars['sigma_yp'] = np.sqrt((beam.py**2).mean())
+    pars['dpp'] = np.sqrt((beam.pt**2).mean())
+    pars['geom_emitt_x'] = np.sqrt((beam.x**2).mean() * (beam.px**2).mean() - (beam.x*beam.px).mean()**2)
+    pars['geom_emitt_y'] = np.sqrt((beam.y**2).mean() * (beam.py**2).mean() - (beam.y*beam.py).mean()**2)
+    pars['emitt_norm_x'] = pars['geom_emitt_x'] * gamma
+    pars['emitt_norm_y'] = pars['geom_emitt_y']  * gamma
+    pars['beta_x'] = pars['sigma_x']**2 / pars['geom_emitt_x']
+    pars['beta_y'] = pars['sigma_y']**2 / pars['geom_emitt_y']
+    pars['alpha_x'] = - (beam.x*beam.px).mean() / pars['geom_emitt_x']
+    pars['alpha_y'] = - (beam.y*beam.py).mean() / pars['geom_emitt_y']
+    pars['gamma_x'] = pars['sigma_xp']**2 / pars['geom_emitt_x']
+    pars['gamma_y'] = pars['sigma_yp']**2 / pars['geom_emitt_y']
+    return pars
